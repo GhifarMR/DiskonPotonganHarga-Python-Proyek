8340c00c2a5f4ada6ff9960ce21d5c2299137ade
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-11-28 17:48:22 +0700

    make simpler

--- Proyek/Discount.py
+++ Proyek/Discount.py
@@ -5,19 +5,16 @@ print('=======================')
 totalBelanja = int(input('Masukkan Total Belanja : '))
 
 if(totalBelanja > 500000) :
-    diskon = 0.3
-    print('Selmaat anda mendapatkan diskon 30%')
+    diskon = 30
 elif(totalBelanja > 250000) :
-    diskon = 0.15
-    print('Selamat anda mendapatkan diskon 15%')
+    diskon = 15
 elif(totalBelanja > 50000) :
-    diskon = 0.05
-    print('Selamat anda mendapatkan diskon 5%')
-elif(totalBelanja < 50000):
+    diskon = 5
+else:
     diskon = 0
 
-totalBayar = totalBelanja - (totalBelanja * diskon)
+print(f'Selamat anda mendapatkan diskon {diskon}%')
+totalBayar = totalBelanja - (totalBelanja * (diskon / 100))
 
 print(f'Total Bayar = {totalBayar}')
 print('=======================')
-
